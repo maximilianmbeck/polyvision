5137a367570d3532f401152aafd311e6007828d5
beckm <beck@ml.jku.at>
2021-08-02 16:37:38 +0200

    Rename to 'polyvision'

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(package_xml_path='/home/max/phd/radar/ws1/src/beamdatagenerator',
-    packages=['beamdatagenerator'],
+setup_args = generate_distutils_setup(package_xml_path='/home/max/phd/radar/ws2/src/polyvision',
+    packages=['polyvision'],
     package_data={'': ['']},
     package_dir={'': 'src'})
 
